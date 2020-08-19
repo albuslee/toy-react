054bfab032be81408254280556c99362f71f1e50
Albus Lee <albuslee@gmail.com>
2020-08-19 23:54:58 +1000

    apply React official tutorial tic tac toe with toy-react & update rerender function

--- toy-react.js
+++ toy-react.js
@@ -7,6 +7,8 @@ class ElementWrapper {
   setAttribute(name, value) {
     if (name.match(/^on([\s\S]+)$/)) {
       this.root.addEventListener(RegExp.$1.replace(/.*/, c => c.toLowerCase()), value)
+    } else if (name === 'className') {
+      this.root.setAttribute('class', value)
     } else {
       this.root.setAttribute(name, value)
     }
@@ -53,8 +55,15 @@ export class Component {
   }
 
   rerender() {
-    this._range.deleteContents()
-    this[RENDER_TO_DOM](this._range)
+    let oldRange = this._range
+
+    let range = document.createRange()
+    range.setStart(oldRange.startContainer, oldRange.startOffset)
+    range.setEnd(oldRange.startContainer, oldRange.startOffset)
+    this[RENDER_TO_DOM](range)
+
+    oldRange.setStart(range.endContainer, range.endOffset)
+    oldRange.deleteContents()
   }
 
   setState(newState) {
@@ -93,6 +102,9 @@ export function createElement(type, attributes, ...children) {
     for (let child of children) {
         if (typeof child === 'string') {
             child = new TextWrapper(child)
+        }
+        if (child === null) {
+          continue
         }
         if (typeof child === 'object' && child instanceof Array) {
           insertChildren(child)
